4b30132136b9d8a027ad5a37a5eb17fa281fb17f
ITALIANI <m.italiani@student.isartdigital.com>
2018-03-30 16:11:48 +0200

    Add basic NodeBasedWindow and Node

--- Assets/Plugins/NodalEditor/Editor/NodeBasedWindow.cs
+++ Assets/Plugins/NodalEditor/Editor/NodeBasedWindow.cs
@@ -1,16 +1,87 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEditor;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class NodeBasedWindow : MonoBehaviour {
+public class NodeBasedWindow : EditorWindow
+{
+    private List<Node> m_nodes = new List<Node>();
+    private GUIStyle m_nodeStyle;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [MenuItem("Window/Window Editor")]
+    private static void OpenWindow()
+    {
+        NodeBasedWindow window = GetWindow<NodeBasedWindow>();
+        window.titleContent = new GUIContent("Window Editor");
+    }
+
+    private void OnGUI()
+    {
+        DrawNodes();
+
+        ProcessNodeEvents(Event.current);
+        ProcessEvents(Event.current);
+
+        if (GUI.changed)
+        {
+            Repaint();
+        }
+    }
+
+    private void DrawNodes()
+    {
+        if ( m_nodes != null )
+        {
+            int size = m_nodes.Count;
+            for ( int idx = 0; idx < size; ++idx )
+            {
+                m_nodes[idx].Draw();
+            }
+        }
+    }
+
+    private void ProcessEvents(Event e)
+    {
+        switch ( e.type )
+        {
+            case EventType.MouseDown:
+                if ( e.button == 1 )
+                {
+                    ProcessContextMenu( e.mousePosition );
+                }
+                break;
+        }
+    }
+
+    private void ProcessNodeEvents(Event e)
+    {
+        if (m_nodes != null)
+        {
+            for (int i = m_nodes.Count - 1; i >= 0; i--)
+            {
+                bool guiChanged = m_nodes[i].ProcessEvents(e);
+
+                if (guiChanged)
+                {
+                    GUI.changed = true;
+                }
+            }
+        }
+    }
+
+    private void ProcessContextMenu(Vector2 mousePosition)
+    {
+        GenericMenu genericMenu = new GenericMenu();
+        genericMenu.AddItem( new GUIContent("Add node"), false, () => OnClickAddNode(mousePosition) );
+        genericMenu.ShowAsContext();
+    }
+
+    private void OnClickAddNode(Vector2 mousePosition)
+    {
+        if ( m_nodes == null )
+        {
+            m_nodes = new List<Node>();
+        }
+
+        m_nodes.Add( new Node( mousePosition, m_nodes.Count ) );
+    }
 }
--- Assets/Plugins/NodalEditor/Scripts/Core/Node.cs
+++ Assets/Plugins/NodalEditor/Scripts/Core/Node.cs
@@ -1,16 +1,73 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEditor;
 using System.Collections.Generic;
-using UnityEngine;
+using Interfaces;
 
-public class Node : MonoBehaviour {
+public class Node : IDrawable
+{
+    private Rect m_rect = new Rect();
+    private string m_title = "No Title";
+    private bool m_isDragged = false;
+    private GUIStyle m_style = new GUIStyle();
+    private int m_id = -1;
+    private List<Port> m_ports = new List<Port>();
+    private List<int> m_modGroup = new List<int>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Node(Vector2 position, int id)
+    {
+        m_rect = new Rect(position.x, position.y, 100, 100);
+        m_style.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/node1.png") as Texture2D;
+        m_style.border = new RectOffset(12, 12, 12, 12);
+        m_id = id;
+    }
+
+    public void Drag(Vector2 delta)
+    {
+        m_rect.position += delta;
+    }
+
+    public void Draw() 
+    {
+        GUI.Box(m_rect, "", m_style);
+        GUI.BeginGroup(m_rect);
+        GUILayout.Label(m_title);
+        GUI.EndGroup();
+    }
+
+
+    public bool ProcessEvents(Event e)
+    {
+        switch (e.type)
+        {
+            case EventType.MouseDown:
+                if (e.button == 0)
+                {
+                    if (m_rect.Contains(e.mousePosition))
+                    {
+                        m_isDragged = true;
+                        GUI.changed = true;
+                    }
+                    else
+                    {
+                        GUI.changed = true;
+                    }
+                }
+                break;
+
+            case EventType.MouseUp:
+                m_isDragged = false;
+                break;
+
+            case EventType.MouseDrag:
+                if (e.button == 0 && m_isDragged)
+                {
+                    Drag(e.delta);
+                    e.Use();
+                    return true;
+                }
+                break;
+        }
+
+        return false;
+    }
 }
