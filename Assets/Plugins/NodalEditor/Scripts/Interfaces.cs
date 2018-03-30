9309923847daa05e2e573172314ddf767ac4dc57
n.fontes <n.fontes@student.isartdigital.com>
2018-03-30 14:19:59 +0100

    Add assets

---
+++ Assets/Plugins/NodalEditor/Scripts/Interfaces.cs
@@ -0,0 +1,32 @@
+﻿namespace Interfaces
+{
+    public interface IDrawable
+    {
+        void Draw();
+    }
+
+    public interface ISaveable
+    {
+        bool Save();
+    }
+
+    public interface IRestorable
+    {
+        bool Restore();
+    }
+
+    public interface IResetable
+    {
+        void Reset();
+    }
+
+    public interface IDeletable
+    {
+        bool Delete();
+    }
+
+    public interface ICreatable
+    {
+        bool Create();
+    }
+}
