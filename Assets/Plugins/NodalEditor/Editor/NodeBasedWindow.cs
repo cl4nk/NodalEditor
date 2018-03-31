128e41ce511dedad915c5d208b741ac2a39c969c
ITALIANI <m.italiani@student.isartdigital.com>
2018-03-31 15:20:38 +0200

    fix window

--- Assets/Plugins/NodalEditor/Editor/NodeBasedWindow.cs
+++ Assets/Plugins/NodalEditor/Editor/NodeBasedWindow.cs
@@ -4,38 +4,15 @@ using System.Collections.Generic;
 
 public class NodeBasedWindow : EditorWindow
 {
-    [MenuItem("Window/Window Editor")]
-    private static void OpenWindow()
-    {
-        NodeBasedWindow window = GetWindow<NodeBasedWindow>();
-        window.titleContent = new GUIContent("Window Editor");
-    }
-
-    private void OnGUI()
-    {
-        DrawNodes();
-
-        ProcessEvents(Event.current);
-
-        if (GUI.changed) Repaint();
-    }
-
-    private void DrawNodes()
-    {
-    }
-
-    private void ProcessEvents(Event e)
-    {
-    }
-    /*   private GUIStyle m_nodeStyle;
-       //private Graph m_graph;
+       private GUIStyle m_nodeStyle;
+       private Graph m_graph;
 
        [MenuItem("Window/Window Editor")]
        private static void OpenWindow()
        {
            NodeBasedWindow window = GetWindow<NodeBasedWindow>();
            window.titleContent = new GUIContent("Window Editor");
-           //window.m_graph = Graph.Create(typeof(Graph));
+           window.m_graph = Graph.Create(typeof(Graph));
        }
 
        private void OnGUI()
@@ -69,8 +46,8 @@ public class NodeBasedWindow : EditorWindow
 
        private void ProcessContextMenu(Vector2 mousePosition)
        {
-           //GenericMenu genericMenu = new GenericMenu();
-           //genericMenu.AddItem( new GUIContent("Add node"), false, () => m_graph.AddNode(mousePosition) );
-           //genericMenu.ShowAsContext();
-       }*/
+           GenericMenu genericMenu = new GenericMenu();
+           genericMenu.AddItem( new GUIContent("Add node"), false, () => m_graph.AddNode(mousePosition) );
+           genericMenu.ShowAsContext();
+       }
 }
